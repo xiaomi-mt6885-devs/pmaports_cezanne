b6db3dc3a79c046f29337dffd1c4db156c912c63
djyee <furkanguzel161@gmail.com>
2022-07-14 15:02:31 +0300

    samsung-treltexx

---
+++ device/testing/linux-samsung-treltexx/preventerror.sh
@@ -0,0 +1,10 @@
+commit="ab88c0ea282670b63059839b9483eb2f29d9d6dd"
+sudo mkdir -m 777 /home/furkan/note4/chroot_native/home/pmos/build/src/android_kernel_samsung_universal5433-$commit/out/
+
+sudo mkdir -m 777 /home/furkan/note4/chroot_native/home/pmos/build/src/android_kernel_samsung_universal5433-$commit/out/drivers
+
+sudo mkdir -m 777 /home/furkan/note4/chroot_native/home/pmos/build/src/android_kernel_samsung_universal5433-$commit/out/drivers/sensorhub
+
+sudo mkdir -m 777 /home/furkan/note4/chroot_native/home/pmos/build/src/android_kernel_samsung_universal5433-$commit/out/drivers/sensorhub/brcm
+
+sudo mkdir -m 777 /home/furkan/note4/chroot_native/home/pmos/build/src/android_kernel_samsung_universal5433-$commit/out/drivers/sensorhub/brcm/factory
